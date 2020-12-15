8ac83f0483fc8f2b681a04fcdb222ab2a0df5f7d
ChloeArd <chloe.ardoise@gmail.com>
2020-12-15 14:32:26 +0100

    Exercice 89

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ if (futur > aujourdhui) {
     let newDiv = document.createElement("div");
 
     let interval = futur.getTime() - aujourdhui.getTime();
-
+//Différence totale
     let days = Math.floor(interval / 60 / 60 / 24 / 1000);
     let hours = Math.floor((interval - (days * 60 * 60 * 24 * 1000)) / 60 / 60 / 1000);
     newDiv.innerHTML = "La différence entre ses 2 dates est de " + days + " jour(s) ET " + hours + " heure(s)."
